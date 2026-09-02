41b7224dc96d08509e5118b54e171359430bc7c8
Tyson-Sujon <osiassujon@gmail.com>
2026-09-02 18:32:56 +0600

    try some code ,not ended

--- even-only.js
+++ even-only.js
@@ -4,5 +4,14 @@
  *
  */
 
-function evenNum(num1) {}
+function evenNum(num1) {
+  for (const element of num1) {
+    let add = [];
+    console.log(element);
+    if (element % 2 === 0) {
+      add = element.unshift();
+    }
+  }
+}
+
 const number = evenNum([12, 3, 44, 5, 64, 31, 42]);
