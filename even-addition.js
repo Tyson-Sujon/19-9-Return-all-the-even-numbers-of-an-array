fe04dcb079004ca92194457c691c02fe423a4c90
Tyson-Sujon <osiassujon@gmail.com>
2026-09-02 21:44:16 +0600

    final update

--- even-addition.js
+++ even-addition.js
@@ -1,8 +1,14 @@
 /**return the sum of even numbers */
 function addEven(num1) {
+  let sum = 0;
   for (const first of num1) {
-    console.log(first);
+    if (first % 2 === 0) {
+      sum = sum + first;
+    }
   }
+  return sum;
 }
 
 const result = addEven([2, 4, 3, 6, 8, 9, 7, 5, 0]);
+
+console.log(result);
--- try.js
+++ try.js
@@ -1,13 +1,28 @@
-function addArray(sum) {
-  let add = [];
-  for (const element of sum) {
-    add = element.unshift(add);
-  }
-  return add;
-}
-const addition = addArray([1, 2, 3, 4]);
+// function addArray(sum) {
+//   let add = [];
+//   for (const element of sum) {
+//     add = element.unshift(add);
+//   }
+//   return add;
+// }
+// const addition = addArray([1, 2, 3, 4]);
 
-console.log("sum of Array", addition);
+// console.log("sum of Array", addition);
 
 // let num=[1,2];
 // let num2 =
+
+/**return the sum of even numbers */
+function addEven(num1) {
+  let sum = 0;
+  for (const first of num1) {
+    if (first % 2 === 0) {
+      sum = sum + first;
+    }
+  }
+  return sum;
+}
+
+const result = addEven([2, 4, 3, 6, 8, 9, 7, 5, 0]);
+
+console.log(result);
